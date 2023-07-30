c66d17bd332289480b623b4c547ddf7efaaeafd6
minsoozz <minsoo1604@naver.com>
2023-07-30 23:49:09 +0900

    feat: Flux 테스트 코드 작성

---
+++ src/test/java/com/github/minsoozz/reactive/ReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.minsoozz.reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
